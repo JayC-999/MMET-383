3f0b4ea65df70d749d7c3ca211405f15c8fb953d
JayC-999 <80921850+JayC-999@users.noreply.github.com>
2021-03-19 01:14:55 -0500

    Add files via upload

---
+++ Lab/Lab5/LoanPaymentCalc.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+    Public LoanAmount As Double = 0
+    Public AnnualIntrestRate As Double = 0
+    Public Years As Double = 0
+    Public Payment As Double = 0
+    Public MonthlyRate As Double = 0
+    Public NumOfPayments As Double = 0
+    Public InterestPayment As Double = 0
+    Public PrinciplaPayment As Double = 0
+    Public Balance As Double = 0
+    Public CurrentPaymentNumber As Double
+    Public Message As String
+
+
+    Private Sub btnPayment_Click(sender As Object, e As EventArgs) Handles btnPayment.Click
+        'payment button
+
+        LoanAmount = txtLoanAmt.Text
+        AnnualIntrestRate = txtAnnualIntrestRate.Text
+        Years = txtYears.Text
+
+        NumOfPayments = Years * 12 'years to months
+        MonthlyRate = AnnualIntrestRate / (12 * 100) ' convert to percentage
+
+        'formula for monthly payments
+        Payment = LoanAmount * MonthlyRate / (1 - (1 + MonthlyRate) ^ (-NumOfPayments))
+
+        'display payment resutl
+        txtPayment.Enabled = True
+        txtPayment.Text = Format(Payment, "##.00")
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'end button
+        End
+    End Sub
+
+    Private Sub btnAmortize_Click(sender As Object, e As EventArgs) Handles btnAmortize.Click
+        'Amortize Button
+
+        'initialize balance
+        Balance = LoanAmount
+
+        'preparing message
+        Message = "Payment" & vbTab & "Total" & vbTab & "Interest" & vbTab & "Principal" & vbTab & "Balance" & vbCrLf &
+                  "Number" & vbTab & "Payment" & vbTab & "Payment" & vbTab & "Payment"
+
+        CurrentPaymentNumber = 1
+        Do
+            'calcualting monthly payments for each month
+            InterestPayment = (AnnualIntrestRate / (12 * 100)) * Balance
+            PrinciplaPayment = Payment - InterestPayment
+            Balance = Balance - PrinciplaPayment
+
+            'message for each month
+            Message = Message & vbCrLf & CurrentPaymentNumber & vbTab & Format(Payment, "0.00") & vbTab & Format(InterestPayment, "0.00") & vbTab &
+                      Format(PrinciplaPayment, "0.00") & vbTab & Format(Balance, "0.00")
+            CurrentPaymentNumber = CurrentPaymentNumber + 1
+        Loop While (CurrentPaymentNumber <= NumOfPayments)
+
+        'output final message
+        txtSchedule.Text = Message
+    End Sub
+End Class
---
+++ Lab/Lab5/MultiTables.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Public BaseNumber As Double = 0
+    Public LowTimes As Double = 0
+    Public HighTimes As Double = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'calculate button
+
+        If txtBaseNum.Text = "" And txtHighTimes.Text = "" And txtLowTimes.Text = "" Then
+            MsgBox("Please make sure all fields have an input")
+        Else
+
+            BaseNumber = txtBaseNum.Text
+            LowTimes = txtLowTimes.Text
+            HighTimes = txtHighTimes.Text
+
+            If LowTimes <= 0 Or HighTimes <= 0 Or BaseNumber <= 0 Then
+                MsgBox("Please make sure all inputs are greater than zero")
+            Else
+                'display for multiplication table
+                ListBox1.Items.Add("Multiplication Table for " & BaseNumber)
+
+                'display results and end on given condition
+                Do
+                    ListBox1.Items.Add(BaseNumber & " X " & LowTimes & " = " & BaseNumber * LowTimes)
+
+                    LowTimes = LowTimes + 1
+
+
+                Loop While (LowTimes <= HighTimes)
+            End If 'end if if all fields are greater than zero
+        End If 'end if for all fields checked
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'End Button
+        End
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear Button
+
+        txtBaseNum.Text = ""
+        txtLowTimes.Text = ""
+        txtHighTimes.Text = ""
+        ListBox1.Items.Clear()
+    End Sub
+End Class
