b378b3e578726e1759612374e328363dcb038558
JayC-999 <80921850+JayC-999@users.noreply.github.com>
2021-03-19 01:22:03 -0500

    Add files via upload

---
+++ Lab/Lab8/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+    'declaring variables
+    Public Quantity As Integer
+    Private LowRange As Integer
+    Private HighRange As Integer
+    Private Message As String
+    Private RandomNumbersArray() As Integer
+
+    Private Sub btnGenData_Click(sender As Object, e As EventArgs) Handles btnGenData.Click
+        'generate buttton
+
+        'condition if erros occur. If none, proceed to generate random numbers
+        If Lable1.Text = "" Or txtLow.Text = "" Or txtHigh.Text = "" Then
+
+            MsgBox("Please make sure all fields are filled")
+
+        ElseIf Not IsNumeric(txtQty.Text) Or Not IsNumeric(txtLow.Text) Or Not IsNumeric(txtHigh.Text) Then
+
+            MsgBox("Please make sure all fields are numeric values and greater than zero")
+        Else
+
+            'variables equal textboxes
+            Quantity = txtQty.Text
+            LowRange = txtLow.Text
+            HighRange = txtHigh.Text
+
+            'preparing message and array
+            Message = "Data has been generated:"
+            ReDim Preserve RandomNumbersArray(0 To Quantity - 1)
+
+            'generates random numbers and stores them in the array
+            For i = 0 To Quantity - 1
+                Dim RanNumber = CInt(Rnd() * (HighRange - LowRange) + LowRange)
+                RandomNumbersArray(i) = RanNumber
+            Next i
+
+            'makes sure that there are 10 integers per line(row)
+            For i = 0 To Quantity - 1
+                If (i Mod 10) = 0 Then
+                    Message = Message & vbCrLf & CStr(RandomNumbersArray(i))
+                Else
+                    Message = Message & vbTab & CStr(RandomNumbersArray(i))
+                End If
+            Next i
+
+            'display data
+            lblStatus.Visible = True
+            txtDisplay.Enabled = True
+            txtDisplay.Text = Message
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        'save button
+
+        lblStatus.Visible = False
+
+        'opens file and stores data to it
+        FileOpen(1, "data.txt", OpenMode.Output)
+
+        'loop for making sure there are 10 integers per line
+        For i = 0 To Quantity - 1
+            If ((i + 1) Mod 10) = 0 Then
+                Write(1, RandomNumbersArray(i))
+                WriteLine(1)
+            Else
+                Write(1, RandomNumbersArray(i))
+            End If
+        Next
+
+        'notifies that user saved file
+        lblStatus.Visible = True
+        lblStatus.Text = "File has been saved"
+        MsgBox("File has been saved")
+
+        'close file
+        FileClose(1)
+    End Sub
+
+    Private Sub btnGoToRead_Click(sender As Object, e As EventArgs) Handles btnGoToRead.Click
+        'got to read button
+
+        Me.Hide()
+        Form2.show()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'exit button
+
+        Me.Close()
+        End
+    End Sub
+End Class
---
+++ Lab/Lab8/Form2.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form2
+    Private Min As Integer
+    Private Max As Integer
+    Private Mean As Decimal
+    Private Sum As Integer
+    Private TotalNumberOfData As Integer
+    Private CurrentDataNumber As Integer
+    Private Data As String
+    Private Sub btnAnalyze_Click(sender As Object, e As EventArgs) Handles btnAnalyze.Click
+        'analyze button
+
+        'open file
+        FileOpen(2, "data.txt", OpenMode.Input)
+
+        'read first data point
+        Input(2, Data)
+        CurrentDataNumber = CInt(Data)
+
+        'initializing variables
+        TotalNumberOfData = 1
+        Min = CurrentDataNumber
+        Max = CurrentDataNumber
+        Sum = CurrentDataNumber
+
+        'reading rest of data
+        Do Until EOF(2)
+            Input(2, Data)
+
+            If Data <> "" Then 'reads 10 numbers per line
+                CurrentDataNumber = CInt(Data)
+
+                'finding max number
+                If CurrentDataNumber >= Max Then
+                    Max = CurrentDataNumber
+                End If
+
+                'finding min number
+                If CurrentDataNumber <= Min Then
+                    Min = CurrentDataNumber
+                End If
+
+                'counting sum and total numbers within the file
+                TotalNumberOfData = TotalNumberOfData + 1
+                Sum = Sum + CurrentDataNumber
+            End If
+        Loop
+
+        'display output
+        txtQty2.Text = Form1.Quantity
+        txtMin.Text = Min
+        txtMax.Text = Max
+        Mean = Sum / TotalNumberOfData
+        txtMean.Text = Format(Mean, "0.00")
+
+        FileClose(2)
+    End Sub
+
+    Private Sub btnGoToWrite_Click(sender As Object, e As EventArgs) Handles btnGoToWrite.Click
+        'go to write button
+
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub btnExit2_Click(sender As Object, e As EventArgs) Handles btnExit2.Click
+        'exit button
+
+        Me.Close()
+        End
+    End Sub
+End Class
