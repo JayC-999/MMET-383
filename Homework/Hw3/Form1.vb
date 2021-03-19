e193a3cacdce2598a7e5f9689c953d7c2dfd6bee
JayC-999 <80921850+JayC-999@users.noreply.github.com>
2021-03-19 01:44:13 -0500

    Add files via upload

---
+++ Homework/Hw3/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Enter button
+
+        QuizScore = TextBox1.Text
+
+        If QuizScore >= 70 Then
+            TotalNumQuizScores = TotalNumQuizScores + QuizScore
+            TotalNumScores = TotalNumScores + 1
+            NumOfCOrBetter = NumOfCOrBetter + 1
+
+            If QuizScore > MaxValue Then 'to find max value 
+                MaxValue = QuizScore
+            ElseIf MaxValue < QuizScore Then 'compares maxvalue to current quize score 
+                MaxValue = QuizScore
+            End If
+
+            If QuizScore < minValue Then 'to find min value
+                minValue = QuizScore
+            ElseIf minValue < QuizScore Then 'compares minvalue to current quize score
+                minValue = minValue
+            End If
+
+        End If
+
+        If QuizScore < 70 Then
+            TotalNumQuizScores = TotalNumQuizScores + QuizScore
+            TotalNumScores = TotalNumScores + 1
+
+            If QuizScore > MaxValue Then 'to find max value
+                MaxValue = QuizScore
+            ElseIf MaxValue < QuizScore Then 'compares maxvalue to current quize score
+                MaxValue = QuizScore
+            End If
+
+            If QuizScore < minValue Then 'to find min value
+                minValue = QuizScore
+            ElseIf minValue < QuizScore Then 'compares minvalue to current quize score
+                minValue = minValue
+            End If
+
+        End If
+
+        'these two operations enables multiple inputs within textbox1
+        TextBox1.Text = ""
+        TextBox1.Focus()
+
+        'output values
+        HighestScore = MaxValue
+        LowestScore = minValue
+        AvgScore = TotalNumQuizScores / TotalNumScores
+
+        Format(AvgScore, "0.00")
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Summary button
+        Form2.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Clear Button
+        TextBox1.Text = ""
+    End Sub
+End Class
---
+++ Homework/Hw3/Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = ModuleQuiz.TotalNumScores
+        TextBox2.Text = ModuleQuiz.HighestScore
+        TextBox3.Text = ModuleQuiz.LowestScore
+        TextBox4.Text = ModuleQuiz.AvgScore
+        TextBox5.Text = ModuleQuiz.NumOfCOrBetter
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+End Class
